504ac38a41330dce1eac4b8092c2652b565c17d7
D0uble3 <1805711826@qq.com>
2026-09-07 15:53:32 +0800

    day2.1

---
+++ learning/输入.py
@@ -0,0 +1,17 @@
+# name=input("请输入您的姓名：")
+# print(f"欢迎您，{name}")
+# #atm取款
+# total=10000
+# #输入密码
+# password=input("请输入您的密码：")
+# if password=="123456":
+#     print("密码正确，请输入取款金额：")
+#     money=int(input())
+#     if money>total:
+#         print("余额不足，取款失败！")
+#     else:
+#         total-=money
+#         print(f"取款成功，当前余额为{total}元")
+num1=(input("请输入第一个数字："))
+num2=(input("请输入第二个数字："))
+print(f"您输入的两个数字的和为：{int(num1)+int(num2)}")
